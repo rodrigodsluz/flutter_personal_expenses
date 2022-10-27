d47a995982eeed16b133cbf551b47f9b3d9bd8ff
Rodrigo Luz <rodrigodsluz@gmail.com>
2022-10-27 16:28:11 -0300

    feat: adding user transactions to the list

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
+  final Function addTx;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +36,8 @@ class NewTransaction extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                onPressed: () {
-                  print(titleController.text);
-                },
+                onPressed: () => addTx(
+                    titleController.text, double.parse(amountController.text)),
               )
             ]),
       ),
--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -27,11 +27,24 @@ class _UserTransactionsState extends State<UserTransactions> {
     ),
   ];
 
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        NewTransaction(),
+        NewTransaction(_addNewTransaction),
         TransactionList(_userTransactions),
       ],
     );
