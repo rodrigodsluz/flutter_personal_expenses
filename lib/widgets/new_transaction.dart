8e78962d07fe433ccf7e96cebba51187e0e296e1
Rodrigo Luz <rodrigodsluz@gmail.com>
2022-10-27 17:52:11 -0300

    feat: further input and output styling and configuration

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,6 +7,20 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -19,26 +33,23 @@ class NewTransaction extends StatelessWidget {
               TextField(
                 decoration: InputDecoration(labelText: 'Title'),
                 controller: titleController,
-                // onChanged: (val) {
-                //   titleInput = val;
-                // },
+                onSubmitted: (_) => submitData(),
               ),
               TextField(
                 decoration: InputDecoration(labelText: 'Amount'),
                 controller: amountController,
-                //onChanged: (val) => amountInput = val,
+                keyboardType: TextInputType.numberWithOptions(signed: true),
+                onSubmitted: (_) => submitData(),
               ),
               TextButton(
-                child: Text('Add Transaction'),
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.purple,
-                  textStyle: TextStyle(
-                    fontWeight: FontWeight.bold,
+                  child: Text('Add Transaction'),
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.purple,
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                onPressed: () => addTx(
-                    titleController.text, double.parse(amountController.text)),
-              )
+                  onPressed: submitData)
             ]),
       ),
     );
