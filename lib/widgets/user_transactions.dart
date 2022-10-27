ebd6177574350c331593466f0238073088455132
Rodrigo Luz <rodrigodsluz@gmail.com>
2022-10-27 17:11:39 -0300

    feat: adding list view builder

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -25,6 +25,24 @@ class _UserTransactionsState extends State<UserTransactions> {
       amount: 16.53,
       date: DateTime.now(),
     ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
   ];
 
   void _addNewTransaction(String txTitle, double txAmount) {
